04e6ea820173f6e2e088d33daf03d177e96201ba
ggors <ggorski@consoneo.com>
2023-06-01 13:00:02 +0200

    ✨ Home page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,21 @@
 import {NavLink} from "react-router-dom";
 import styles from "../assets/css/NavBar.module.css";
+import logo from "../assets/images/logo.png"
 
 function NavBar() {
 
     return (
         <nav className={styles.navBar}>
+            <div>
+                <img src={logo} alt="logo"/>
+            </div>
             <NavLink to="/">
                 Home
             </NavLink>
             <NavLink to="/contact">
                 Contact
             </NavLink>
+            <div className={styles.background}></div>
         </nav>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
+import styles from "../assets/css/Home.module.css";
 import carpenter from "../assets/images/callum-hill-oamw52SCGi0-unsplash.jpg"
 
 function Home() {
 
     return (
-        <section>
-        </section>
+        <>
+            <section className={styles.home_hero_section}>
+                <div className={styles.home_title}>
+                    <h1>Welcome to the Frenchman Carpentry workshop</h1>
+                    <h2>My promise as an independent carpenter is to transform your vision into meticulously crafted
+                        structures
+                        that stand the test of time.
+                    </h2>
+                </div>
+            </section>
+
+        </>
     )
 }
 
