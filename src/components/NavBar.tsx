c5688952d90701a175b1ef9c0dc4b0d5d70c1943
ggors <ggorski@consoneo.com>
2023-06-01 18:20:48 +0200

    💄 Effect on navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,39 @@
 import {NavLink} from "react-router-dom";
 import styles from "../assets/css/NavBar.module.css";
 import logo from "../assets/images/logo.png"
+import {useEffect, useState} from "react";
 
 function NavBar() {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isScrolledAfter, setIsScrolledAfter] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const handleScroll = () => {
+        if (window.scrollY > 10) {
+            setIsScrolled(true);
+        } else {
+            setIsScrolled(false);
+            setIsScrolledAfter(false); // Ajout de cette ligne pour réinitialiser isScrolledAfter
+        }
+
+        if (window.scrollY > 820) {
+            setIsScrolledAfter(true);
+        } else {
+            setIsScrolledAfter(false);
+        }
+        console.log(window.scrollY);
+    };
+
 
     return (
-        <nav className={styles.navBar}>
+        <nav
+            className={`${styles.navBar} ${isScrolled ? styles.scrolled : ""} ${isScrolledAfter ? styles.scrolledAfter : ""}`}>
             <div>
                 <img src={logo} alt="logo"/>
             </div>
