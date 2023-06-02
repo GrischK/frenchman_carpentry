4d33cb0759b053f5b028c2285a8045900d833b2d
ggors <ggorski@consoneo.com>
2023-06-02 12:34:14 +0200

    ✨  Animation on text

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -27,7 +27,7 @@ export default function Reveal({children}: RevealProps) {
                 }}
                 initial="hidden"
                 animate={mainControls}
-                transition={{duration: 0.5, delay: 0.75}}
+                transition={{duration: 0.5, delay: 0.25}}
             >
                 {children}
             </motion.div>
