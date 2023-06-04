0bf9a9874c040d413b9c679c1641fdb3fbd14bec
ggors <ggorski@consoneo.com>
2023-06-04 10:50:25 +0200

    ✨  Marquee effect on services title

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,12 +1,19 @@
-import {useScroll, useTransform} from "framer-motion";
+import {useScroll, useTransform, motion} from "framer-motion";
 import styles from "../assets/css/Marque.module.css";
 
 export default function Marquee() {
     const {scrollYProgress} = useScroll()
-    const x = useTransform(scrollYProgress, [0, 1], [0, -600])
+    const x = useTransform(scrollYProgress, [0, 1], [100, -600])
+    const x1 = useTransform(scrollYProgress, [0, 1], [-100, 600])
 
 
-    return (
-        <h2 className={styles.marquee_title}>Discover my services</h2>
+    return (<>
+            <motion.h2 style={{x}} className={styles.marquee_title}>
+                Discover my services
+            </motion.h2>
+            <motion.h2 style={{x: x1}} className={styles.marquee_title}>
+                Discover my services
+            </motion.h2>
+        </>
     )
 }
