4903377577863143b341059a45299fe182c90296
GrischK <grischka.gorski@laposte.net>
2026-02-19 00:21:31 +0100

    ⬆️ Update packages

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+// eslint.config.js
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+
+      "react-refresh/only-export-components": "warn",
+      "no-undef": "off",
+      "@typescript-eslint/no-var-requires": "off",
+    },
+  },
+];
